319f817e3eba8dc1a83595f4df62daaf3bfe8a78
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2022-01-03 12:03:31 -0500

    Add files via upload

---
+++ testing/exmp.py
@@ -0,0 +1,23 @@
+
+"""
+Chris Francis
+
+PyDoc3 Notation Example
+
+(today's date)
+
+Python 3.9.2
+"""
+
+def double(x):
+    """
+    This function will return twice as much as the value passed into it
+    Args:
+        x (int): the number that needs to be doubled
+    Returns:
+        the doubled value of what was passed in
+    """
+    return 2 * x
+
+if __name__=="__main__":
+    print(double(1))
