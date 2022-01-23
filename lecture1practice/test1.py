8e7ef43f1946909c8cb3e817c54332e1f43e7079
unknown <francis.chris5@gmail.com>
2022-01-23 04:32:51 -0500

    practice for next lecture

---
+++ lecture1practice/test1.py
@@ -0,0 +1,8 @@
+
+
+def SayHi():
+    print("Hello World!!!")
+
+
+if __name__=="__main__":
+    SayHi()
---
+++ lecture1practice/test2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 04:16:45 2022
+
+a new line in the template
+
+@author: Christopher S. Francis
+"""
+
+
+def double(x):
+    return 2*x
+
+
+# @todo change this value
+value = 5
+
+if __name__=="__main__":
+    print(double(value))
+
