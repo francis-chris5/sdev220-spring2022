33d248a30906f79338c903c61a96bc7b3133ad96
unknown <francis.chris5@gmail.com>
2022-01-25 19:07:28 -0500

    first part of class example

---
+++ class-125/anothertest.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 18:57:26 2022
+
+a new line in the template
+
+@author: Christopher S. Francis
+"""
+
+
+
+
+# @todo make this a quadurple method instead
+
+def triple(x):
+    return 3 * x
+
+
+if __name__=="__main__":
+    
+    num = 21
+    print(triple(num))
+    
---
+++ class-125/testing123.py
@@ -0,0 +1,7 @@
+
+def double(x):
+    return 2 * x
+
+
+if __name__=="__main__":
+    print(double(15))
