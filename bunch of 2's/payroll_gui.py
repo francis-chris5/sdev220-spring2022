3a217463c0dbb132480164fab35713c9e96e5bc9
unknown <francis.chris5@gmail.com>
2022-02-24 19:45:25 -0500

    second day with tk

--- bunch of 2's/payroll_gui.py
+++ bunch of 2's/payroll_gui.py
@@ -12,6 +12,7 @@ sys.path.insert(0, "C:\\Users\\franc\\Documents\\delete\\git-stuff\\pyOffice")
 
 
 import tkinter as tk
+import tkinter.ttk as ttk
 from employees import *
 
 employees = [
@@ -24,7 +25,7 @@ REGULAR_TIME = 40
 
 def GrossPay(event):
     hours=35
-    choice = int(txtEmployee.get())
+    choice = int(txtBob.get())
     employee = employees[choice]
     if isinstance(employee, Manager):
         hours = REGULAR_TIME
@@ -32,10 +33,12 @@ def GrossPay(event):
         gross = hours * employee.getRate()
     else:
         gross = REGULAR_TIME * employee.getRate() + 1.5 * (hours - REGULAR_TIME) * employee.getRate()
-    lblInstructions["text"] = gross
+    lblBob["text"] = gross
 
 
 
+def UpdateStatus(event, status):
+    lblStatus["text"] = "Status: " + status
 
 
 
@@ -53,14 +56,55 @@ mnPay.add_command(label="Gross Pay", command= lambda : GrossPay(None))
 mnMain.add_cascade(label="Payroll", menu=mnPay)
 
 
-lblInstructions = tk.Label(gui, text="choose employee 0 - 2:")
-txtEmployee = tk.Entry(gui)
+tbMain = tk.Frame(gui, border=3, relief="groove")
+imgGrossPay = tk.PhotoImage(file="gross.png")
+imgGrossPay = imgGrossPay.subsample(2) ##subsample divides by a number
+btnGrossPay = tk.Button(tbMain, image=imgGrossPay) 
+
+
+
+nbMain = ttk.Notebook(gui)
+
+tabBob = ttk.Frame(nbMain)
+lblBob = tk.Label(tabBob, text="Enter Bob's Hours")
+txtBob = tk.Entry(tabBob)
+nbMain.add(tabBob, text="Bob")
+
+tabSally = ttk.Frame(nbMain)
+lblSally = tk.Label(tabSally, text="Enter Sally's Hours")
+txtSally = tk.Entry(tabSally)
+nbMain.add(tabSally, text="Sally")
+
+tabBecky = ttk.Frame(nbMain)
+lblBecky = tk.Label(tabBecky, text="Enter Becky's Hours")
+txtBecky = tk.Entry(tabBecky)
+nbMain.add(tabBecky, text="Becky")
+
+sbMain = tk.Frame(gui, border=3, relief="sunken")
+
+lblStatus = tk.Label(sbMain, text="Status: ready....")
+
+
+
+
+
 
 gui.config(menu=mnMain)
+tbMain.pack(fill="both")
+nbMain.pack(fill="both", expand=True)
+sbMain.pack(fill="both")
 
-lblInstructions.pack(padx=10, pady=45)
-txtEmployee.pack(padx=10, pady=23)
+btnGrossPay.grid(row=0, column=0, padx=5, pady=5)
 
+lblBob.grid(row=0, column=0, padx=10, pady=45)
+txtBob.grid(row=1, column=0, padx=10, pady=23)
+lblSally.grid(row=0, column=0, padx=10, pady=45)
+txtSally.grid(row=1, column=0, padx=10, pady=23)
+lblBecky.grid(row=0, column=0, padx=10, pady=45)
+txtBecky.grid(row=1, column=0, padx=10, pady=23)
+
+
+lblStatus.grid(row=0, column=0)
 
 
 
